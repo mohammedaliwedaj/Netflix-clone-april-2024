97615a245baac04947d760a2706e62388f6325d3
Mohammed Ali <mohammedaliwedaj@gmail.com>
2024-08-31 23:16:26 +0300

    Header and Footer

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>It is working</h1>
+   <div className='App'>
+    <Home/>
+
+   </div>
      
     </>
   )

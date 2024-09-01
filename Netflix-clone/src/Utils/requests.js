2bff7c3027dde42d5d5ecc5cef01f007c08ef909
Mohammed Ali <mohammedaliwedaj@gmail.com>
2024-09-01 22:59:17 +0300

    banner

--- Netflix-clone/src/Utils/requests.js
+++ Netflix-clone/src/Utils/requests.js
@@ -1,8 +1,8 @@
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY=process.env.REACT_APP_API_KEY
 const requests = {
   fetchTrending: "/trending/all/week?api_key=${API_KEY}&language=enUS",
-  fetchNetflixOriginals: "/discover/tv/?api_key=${API_KEY}&with_networks=18",
-  fetchTopRatedMovies:"/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1",
+  fetchNetflixOriginals: "/discover/tv?api_key=${API_KEY}&with_networks=213",
+  fetchTopRatedMovies:    "/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1",
   fetchActionMovies: "/discover/movie?api_key=${API_KEY}&with_genres=28",
   fetchComedyMovies: "/discover/movie?api_key=${API_KEY}&with_genres=35",
   fetchHorrorMovies: "/discover/movie?api_key=${API_KEY}&with_genres=27",
@@ -13,4 +13,4 @@ const requests = {
   fetchHistoryMovies: "/discover/movie?api_key=${API_KEY}&with_genres=36",
   fetchTvShow: "/discover/movie?api_key=${API_KEY}&with_genres=10770",
 };
-export default requests
+export default requests;

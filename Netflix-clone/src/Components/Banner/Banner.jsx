2230e705048164cc2527dbe247109378b86167da
Mohammed Ali <mohammedaliwedaj@gmail.com>
2024-09-02 23:29:13 +0300

    deploy

--- Netflix-clone/src/Components/Banner/Banner.jsx
+++ Netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../../utils/axios";
+import axios from "../../Utils/axios";
 import requests from '../../Utils/requests'
 import "./banner.css";
 
